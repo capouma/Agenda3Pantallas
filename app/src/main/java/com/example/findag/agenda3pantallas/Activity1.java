19e09b938571f15b52950f81815d21a3f3d7235b
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-11 09:13:32 +0100

    Creamos  los metodos  necesarios e instanciamos los objetos que necesitaremos para que esta primera pantalla haga su funcionamiento, es decir, agregue contactos a un array y nos mande a la segunda pantalla para listar contactos.

--- app/src/main/java/com/example/findag/agenda3pantallas/Activity1.java
+++ app/src/main/java/com/example/findag/agenda3pantallas/Activity1.java
@@ -1,17 +1,91 @@
 package com.example.findag.agenda3pantallas;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class Activity1 extends ActionBarActivity {
+
+
+public class Activity1 extends Activity
+{
+
+    // Definimos los dos EditText ( nombre y telefono)
+    EditText edtNombre;
+    EditText edtTelefono;
+
+    // Definimos las variables que nos serviran para el control de nuestra Toast.
+    final int noNomTelf = 1;
+    final int noMatch = 2;
+
+    // Definimos el ArrayList en el que guardaremos nuestros contactos.
+    ArrayList<Contactos> insertContac = new ArrayList<Contactos>();
+
+    //Definimos nuestra variable static que sera nuestro requestCode(esta variable es la que identifica el intent que enviamos).
+    private final static int CONTACTOS = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity1);
+
+
+        // Definimos los botones Guardar y Listar.
+        Button btnGuardar = (Button) findViewById(R.id.btnGuardar);
+        Button btnListar = (Button) findViewById(R.id.btnListar);
+
+        // Definimos el onClick del boton Guardar.
+        btnGuardar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                edtNombre = (EditText) findViewById(R.id.edtNombre);
+                edtTelefono = (EditText) findViewById(R.id.edtTelefono);
+                //Comprobamos que tanto el campo nombre y telefono estan cubiertos, si n lo estan salta la Toast.
+                if (edtNombre.getText().toString().equals("") || edtTelefono.getText().toString().equals(""))
+                {
+                    showToast(noNomTelf);
+                    return;
+                }
+                else
+                {
+                    insertContac.add(new Contactos(edtNombre.getText().toString(), edtTelefono.getText().toString()));
+
+                    //Dejamos los campos del nombre y el telefono en blanco
+                    edtNombre.setText("");
+                    edtTelefono.setText("");
+                }
+
+            }
+        });
+
+        // Definimos el onClick del boton Listar
+        btnListar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent contactos = new Intent(Activity1.this, Activity2.class);
+
+                contactos.putExtra("lista",insertContac);
+
+                startActivityForResult(contactos, CONTACTOS);
+
+            }
+        });
+
+
+
+
     }
 
 
@@ -36,4 +110,43 @@ public class Activity1 extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // Con el metodo onActivityResult recogeremos el valor del intent de la Activity3
+    protected void onActivityResult (int requestCode, int resultCode, Intent listaFinal)
+    {
+        if (resultCode == RESULT_OK)
+        {
+            insertContac = (ArrayList<Contactos>)listaFinal.getExtras().getSerializable("nuevaLista");
+        }
+
+
+    }
+
+    // Este metodo recibira un entero que dependiendo del valor que lleve conseguiremos que se nos muestre una Toast con uno u otro mensaje.
+    public void showToast(int comprueba)
+    {
+        Context context = getApplicationContext();
+
+        int duracion = Toast.LENGTH_SHORT;
+
+        CharSequence texto;
+
+        Toast toast=null;
+
+        if(comprueba == noNomTelf)
+        {
+            texto = getResources().getString(R.string.noNomTelf);
+            toast = Toast.makeText(context, texto, duracion);
+        }
+
+        else if(comprueba == noMatch)
+        {
+            texto = getResources().getString(R.string.noMatch);
+            toast = Toast.makeText(context, texto, duracion);
+        }
+
+        toast.show();
+    }
+
+
 }
