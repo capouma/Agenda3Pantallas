aa979aecda1f2278c3b2023ca3a2955a9782a8a1
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-11 09:16:13 +0100

    Creamos la clase Contactos, necesitamos que sea serializable para poder realizar acciones en diversas pantallas con nuestro ArrayList

---
+++ app/src/main/java/com/example/findag/agenda3pantallas/Contactos.java
@@ -0,0 +1,51 @@
+package com.example.findag.agenda3pantallas;
+
+import java.io.Serializable;
+
+/**
+ * Created by findag on 23/11/14.
+ */
+public class Contactos implements Serializable
+{
+    // Definimos las variables que tendremos.
+    private String nombre;
+    private String telefono;
+
+    // Creamos un constructor vacio.
+    public Contactos()
+    {
+
+    }
+
+    // Creamos un constructor con parametros.
+    public Contactos(String nombre, String telefono)
+    {
+        this.nombre = nombre;
+        this.telefono = telefono;
+
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    // Sobreescribimos el metodo toString para poder visualizar los datos de nuestro ArrayList
+    @Override
+    public String toString() {
+        return nombre + " " + telefono;
+    }
+}
